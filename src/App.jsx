3fc519ce922b3517e0ea55aa7ae7606070a30a04
K-Wathsala <kvwathsala@gmail.com>
2024-11-25 15:58:09 +0530

    update 1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
+
+function App() {
+ 
+  return (
+    <>
+      <Navbar />
+       <main >
+          <Home />
+       </main>
+       {/* footer */}
+    </>
+  )
+}
+
+export default App
